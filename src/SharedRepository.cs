2d59d5d3b61172764c4dfc6bffaa638c7e448228
Jake Soenneker <jake@soenneker.com>
2026-01-02 14:22:41 -0600

    Deps

--- src/SharedRepository.cs
+++ src/SharedRepository.cs
@@ -14,6 +14,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Soenneker.Documents.Typed;
 using Soenneker.Extensions.ValueTask;
+using Soenneker.Utils.MemoryStream.Abstract;
 
 namespace Soenneker.Cosmos.Repositories.Shared;
 
@@ -23,17 +24,20 @@ public abstract class SharedRepository<TDocument> : CosmosRepository<TDocument>,
     protected abstract string EntityType { get; }
 
     protected SharedRepository(ICosmosContainerUtil cosmosContainerUtil, IConfiguration config, ILogger<SharedRepository<TDocument>> logger,
-        IUserContext userContext, IBackgroundQueue backgroundQueue) : base(cosmosContainerUtil, config, logger, userContext, backgroundQueue)
+        IUserContext userContext, IBackgroundQueue backgroundQueue, IMemoryStreamUtil memoryStreamUtil) : base(cosmosContainerUtil, config, logger, userContext,
+        backgroundQueue, memoryStreamUtil)
     {
     }
 
     public override async ValueTask<List<TDocument>> GetAll(double? delayMs = null, CancellationToken cancellationToken = default)
     {
-        IQueryable<TDocument> query = await BuildQueryable(null, cancellationToken).NoSync();
+        IQueryable<TDocument> query = await BuildQueryable(null, cancellationToken)
+            .NoSync();
 
         query = query.Where(c => c.EntityType == EntityType);
 
-        return await GetItems(query, delayMs, cancellationToken).NoSync();
+        return await GetItems(query, delayMs, cancellationToken)
+            .NoSync();
     }
 
     public override async ValueTask<List<IdPartitionPair>> GetAllIds(double? delayMs = null, CancellationToken cancellationToken = default)
@@ -42,7 +46,8 @@ public abstract class SharedRepository<TDocument> : CosmosRepository<TDocument>,
 
         query = query.Where(c => c.EntityType == EntityType);
 
-        return await GetIds(query, delayMs, cancellationToken).NoSync();
+        return await GetIds(query, delayMs, cancellationToken)
+            .NoSync();
     }
 
     public override async ValueTask DeleteAll(double? delayMs = null, bool useQueue = false, CancellationToken cancellationToken = default)
@@ -50,9 +55,11 @@ public abstract class SharedRepository<TDocument> : CosmosRepository<TDocument>,
         Logger.LogWarning("-- COSMOS: {method} (General.{type}Document) w/ {delayMs}ms delay between docs", MethodUtil.Get(), EntityType,
             delayMs.GetValueOrDefault());
 
-        List<IdPartitionPair> ids = await GetAllIds(delayMs, cancellationToken).NoSync();
+        List<IdPartitionPair> ids = await GetAllIds(delayMs, cancellationToken)
+            .NoSync();
 
-        await DeleteIds(ids, delayMs, useQueue, cancellationToken).NoSync();
+        await DeleteIds(ids, delayMs, useQueue, cancellationToken)
+            .NoSync();
 
         Logger.LogDebug("-- COSMOS: Finished {method} (General.{type}Document)", MethodUtil.Get(), EntityType);
     }
@@ -63,23 +70,26 @@ public abstract class SharedRepository<TDocument> : CosmosRepository<TDocument>,
         Logger.LogWarning("-- COSMOS: {method} (General.{type}) w/ {delayMs}ms delay between docs", MethodUtil.Get(), typeof(TDocument).Name,
             delayMs.GetValueOrDefault());
 
-        IQueryable<TDocument> query = await BuildPagedQueryable(pageSize, cancellationToken: cancellationToken).NoSync();
+        IQueryable<TDocument> query = await BuildPagedQueryable(pageSize, cancellationToken: cancellationToken)
+            .NoSync();
         query = query.Where(c => c.EntityType == EntityType);
         query = query.OrderBy(c => c.CreatedAt);
 
         var newQuery = query.Select(c => new { c.DocumentId, c.PartitionKey });
 
         await ExecuteOnGetItemsPaged(newQuery, async results =>
-        {
-            Logger.LogDebug("Number of rows to be deleted in page: {rows}", results.Count);
-
-            foreach (var result in results)
             {
-                cancellationToken.ThrowIfCancellationRequested();
+                Logger.LogDebug("Number of rows to be deleted in page: {rows}", results.Count);
 
-                await DeleteItem(result.DocumentId, result.PartitionKey, useQueue, cancellationToken).NoSync();
-            }
-        }, cancellationToken).NoSync();
+                foreach (var result in results)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    await DeleteItem(result.DocumentId, result.PartitionKey, useQueue, cancellationToken)
+                        .NoSync();
+                }
+            }, cancellationToken)
+            .NoSync();
 
         Logger.LogDebug("-- COSMOS: Finished {method} (General.{type})", MethodUtil.Get(), typeof(TDocument).Name);
     }
